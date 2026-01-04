df70843b2f9c466112f5c3dbff62aed493caccdb
HassanNd <hassannasseraldeenlogin@gmail.com>
2026-01-05 01:48:35 +0200

    Fix start command (correct entry file)

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://hassannasseraldeen:Hasoun313@cluster0.tupupn8.mongodb.net/Smart_Traffic_system");
+    await mongoose.connect("mongodb+srv://hassannasseraldeen:Hasoun313@cluster0.tupupn8.mongodb.net/Smart_Traffic_system?retryWrites=true&w=majority&appName=Cluster0");
     console.log("✅ MongoDB Connected");
   } catch (error) {
     console.error("❌ MongoDB Error:", error.message);
